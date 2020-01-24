18c2cd2c90eb976e42352097630378584cc531c5
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2020-01-24 09:39:13 +0200

    Updates

---
+++ precache-manifest.87990d84442e77f965aa8a163054c8de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e827c78b9575ff3b658036f15b21fb2f",
+    "url": "/home-manager/index.html"
+  },
+  {
+    "revision": "26ef37d3a6ce353795a4",
+    "url": "/home-manager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c30ccc4a00448c683e7f",
+    "url": "/home-manager/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "26ef37d3a6ce353795a4",
+    "url": "/home-manager/static/js/2.b6d7bbdb.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/home-manager/static/js/2.b6d7bbdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c30ccc4a00448c683e7f",
+    "url": "/home-manager/static/js/main.44d08082.chunk.js"
+  },
+  {
+    "revision": "3833df5e5531979b6315",
+    "url": "/home-manager/static/js/runtime-main.0b99892a.js"
+  },
+  {
+    "revision": "72bf702c989547f2eb4bd7a5f445f408",
+    "url": "/home-manager/static/media/bakery.72bf702c.jpg"
+  },
+  {
+    "revision": "9e2e16c48d25a55bc069edae28366033",
+    "url": "/home-manager/static/media/dairy.9e2e16c4.jpg"
+  },
+  {
+    "revision": "72bdaaa0f8f2639d323c476bd725c475",
+    "url": "/home-manager/static/media/drinks.72bdaaa0.jpg"
+  },
+  {
+    "revision": "ad2d7c5baa06c084cc83875198d4ef7a",
+    "url": "/home-manager/static/media/fish.ad2d7c5b.jpg"
+  },
+  {
+    "revision": "0b2a4c353fca2d3194266af7db15b9e1",
+    "url": "/home-manager/static/media/fruits_vegetables.0b2a4c35.jpg"
+  },
+  {
+    "revision": "33a127a601cb6dabe27d4fc898ac422f",
+    "url": "/home-manager/static/media/meat.33a127a6.jpg"
+  },
+  {
+    "revision": "cd07bd0aba62c8ecd38ebdb11703693b",
+    "url": "/home-manager/static/media/no-image-available.cd07bd0a.png"
+  }
+]);
