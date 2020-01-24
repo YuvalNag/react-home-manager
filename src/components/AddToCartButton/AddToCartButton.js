871c56e15d456fe9872af4a797420233239f84a8
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2020-01-24 15:05:20 +0200

    Update AddToCartButton.js

--- src/components/AddToCartButton/AddToCartButton.js
+++ src/components/AddToCartButton/AddToCartButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import { FaOpencart } from "react-icons/fa";
 
 const addToCartButton = props => (
 
-    <Button className='mt-5 mt-sm-1'>Add To Cart</Button>
+    <Button className='mt-5 mt-sm-1'><FaOpencart/> Add To Cart</Button>
 
 )
 
