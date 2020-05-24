e6e34cd10c788d55c9b513b7cb9d2b385c162514
Nissan <nissy34@gmail.com>
2020-05-24 13:48:45 +0300

    Update variables.js

--- src/shared/variables.js
+++ src/shared/variables.js
@@ -1,2 +1,2 @@
-export const apiBaseURL = "https://api." + "heifetz.duckdns.org"
+export const apiBaseURL = "https://api.housekeeper.duckdns.org"
 export const staticBaseUrl = window.location.protocol + '//' + window.location.host
